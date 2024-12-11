cef1a4f6a737c40ccc82a9ac3f55332f139768c5
Ruili Hu <ruili.hu@edu.sait.ca>
2024-12-10 22:33:37 -0700

    Add category page UI

---
+++ CategoryUI.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace EquipmentRentalManagementPrototype
+{
+    partial class CategoryUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.CategoryPageTitle = new System.Windows.Forms.Label();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.SysMgrBtn = new System.Windows.Forms.Button();
+            this.EquipRentMgrBtn = new System.Windows.Forms.Button();
+            this.RentMgrBtn = new System.Windows.Forms.Button();
+            this.CustomMgrBtn = new System.Windows.Forms.Button();
+            this.CatMgrBtn = new System.Windows.Forms.Button();
+            this.EquipMgrBtn = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.DisplayAll = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OperatorText = new System.Windows.Forms.Label();
+            this.OperatorLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.CategoryPageTitle);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainerTop_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 72;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // CategoryPageTitle
+            // 
+            this.CategoryPageTitle.AutoSize = true;
+            this.CategoryPageTitle.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CategoryPageTitle.Location = new System.Drawing.Point(201, 9);
+            this.CategoryPageTitle.Name = "CategoryPageTitle";
+            this.CategoryPageTitle.Size = new System.Drawing.Size(403, 49);
+            this.CategoryPageTitle.TabIndex = 0;
+            this.CategoryPageTitle.Text = "Category Management";
+            this.CategoryPageTitle.Click += new System.EventHandler(this.CategoryPageTitle_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.SysMgrBtn);
+            this.splitContainer2.Panel1.Controls.Add(this.EquipRentMgrBtn);
+            this.splitContainer2.Panel1.Controls.Add(this.RentMgrBtn);
+            this.splitContainer2.Panel1.Controls.Add(this.CustomMgrBtn);
+            this.splitContainer2.Panel1.Controls.Add(this.CatMgrBtn);
+            this.splitContainer2.Panel1.Controls.Add(this.EquipMgrBtn);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.Add);
+            this.splitContainer2.Panel2.Controls.Add(this.textBox3);
+            this.splitContainer2.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer2.Panel2.Controls.Add(this.label5);
+            this.splitContainer2.Panel2.Controls.Add(this.label4);
+            this.splitContainer2.Panel2.Controls.Add(this.label3);
+            this.splitContainer2.Panel2.Controls.Add(this.listBox1);
+            this.splitContainer2.Panel2.Controls.Add(this.label2);
+            this.splitContainer2.Panel2.Controls.Add(this.Search);
+            this.splitContainer2.Panel2.Controls.Add(this.DisplayAll);
+            this.splitContainer2.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer2.Panel2.Controls.Add(this.label1);
+            this.splitContainer2.Panel2.Controls.Add(this.OperatorText);
+            this.splitContainer2.Panel2.Controls.Add(this.OperatorLabel);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 374);
+            this.splitContainer2.SplitterDistance = 174;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // SysMgrBtn
+            // 
+            this.SysMgrBtn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SysMgrBtn.Location = new System.Drawing.Point(3, 280);
+            this.SysMgrBtn.Name = "SysMgrBtn";
+            this.SysMgrBtn.Size = new System.Drawing.Size(168, 41);
+            this.SysMgrBtn.TabIndex = 5;
+            this.SysMgrBtn.Text = "System Mgr";
+            this.SysMgrBtn.UseVisualStyleBackColor = true;
+            // 
+            // EquipRentMgrBtn
+            // 
+            this.EquipRentMgrBtn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EquipRentMgrBtn.Location = new System.Drawing.Point(3, 233);
+            this.EquipRentMgrBtn.Name = "EquipRentMgrBtn";
+            this.EquipRentMgrBtn.Size = new System.Drawing.Size(168, 41);
+            this.EquipRentMgrBtn.TabIndex = 4;
+            this.EquipRentMgrBtn.Text = "Report Mgr";
+            this.EquipRentMgrBtn.UseVisualStyleBackColor = true;
+            // 
+            // RentMgrBtn
+            // 
+            this.RentMgrBtn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RentMgrBtn.Location = new System.Drawing.Point(3, 186);
+            this.RentMgrBtn.Name = "RentMgrBtn";
+            this.RentMgrBtn.Size = new System.Drawing.Size(168, 41);
+            this.RentMgrBtn.TabIndex = 3;
+            this.RentMgrBtn.Text = "Rental Mgr";
+            this.RentMgrBtn.UseVisualStyleBackColor = true;
+            // 
+            // CustomMgrBtn
+            // 
+            this.CustomMgrBtn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomMgrBtn.Location = new System.Drawing.Point(3, 139);
+            this.CustomMgrBtn.Name = "CustomMgrBtn";
+            this.CustomMgrBtn.Size = new System.Drawing.Size(168, 41);
+            this.CustomMgrBtn.TabIndex = 2;
+            this.CustomMgrBtn.Text = "Customer Mgr";
+            this.CustomMgrBtn.UseVisualStyleBackColor = true;
+            // 
+            // CatMgrBtn
+            // 
+            this.CatMgrBtn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CatMgrBtn.Location = new System.Drawing.Point(3, 92);
+            this.CatMgrBtn.Name = "CatMgrBtn";
+            this.CatMgrBtn.Size = new System.Drawing.Size(168, 41);
+            this.CatMgrBtn.TabIndex = 1;
+            this.CatMgrBtn.Text = "Category Mgr";
+            this.CatMgrBtn.UseVisualStyleBackColor = true;
+            // 
+            // EquipMgrBtn
+            // 
+            this.EquipMgrBtn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EquipMgrBtn.Location = new System.Drawing.Point(3, 45);
+            this.EquipMgrBtn.Name = "EquipMgrBtn";
+            this.EquipMgrBtn.Size = new System.Drawing.Size(168, 41);
+            this.EquipMgrBtn.TabIndex = 0;
+            this.EquipMgrBtn.Text = "Equipment Mgr";
+            this.EquipMgrBtn.UseVisualStyleBackColor = true;
+            // 
+            // Search
+            // 
+            this.Search.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Search.Location = new System.Drawing.Point(129, 83);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(84, 26);
+            this.Search.TabIndex = 6;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = false;
+            // 
+            // DisplayAll
+            // 
+            this.DisplayAll.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.DisplayAll.Location = new System.Drawing.Point(18, 83);
+            this.DisplayAll.Name = "DisplayAll";
+            this.DisplayAll.Size = new System.Drawing.Size(84, 26);
+            this.DisplayAll.TabIndex = 5;
+            this.DisplayAll.Text = "Display All";
+            this.DisplayAll.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(18, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Search By Category ID";
+            // 
+            // OperatorText
+            // 
+            this.OperatorText.AutoSize = true;
+            this.OperatorText.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OperatorText.Location = new System.Drawing.Point(415, 13);
+            this.OperatorText.Name = "OperatorText";
+            this.OperatorText.Size = new System.Drawing.Size(68, 18);
+            this.OperatorText.TabIndex = 2;
+            this.OperatorText.Text = "Operator:";
+            // 
+            // OperatorLabel
+            // 
+            this.OperatorLabel.AutoSize = true;
+            this.OperatorLabel.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OperatorLabel.Location = new System.Drawing.Point(489, 15);
+            this.OperatorLabel.Name = "OperatorLabel";
+            this.OperatorLabel.Size = new System.Drawing.Size(80, 15);
+            this.OperatorLabel.TabIndex = 1;
+            this.OperatorLabel.Text = "Not Logged In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Search Result";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(19, 156);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 196);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(356, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 22);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Category Information";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(309, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Category ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(309, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Category Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(448, 126);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 22);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Location = new System.Drawing.Point(448, 167);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 22);
+            this.textBox3.TabIndex = 13;
+            // 
+            // Add
+            // 
+            this.Add.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Add.Location = new System.Drawing.Point(399, 224);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(84, 26);
+            this.Add.TabIndex = 14;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = false;
+            // 
+            // CategoryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "CategoryUI";
+            this.Text = "CategoryUI";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label CategoryPageTitle;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button SysMgrBtn;
+        private System.Windows.Forms.Button EquipRentMgrBtn;
+        private System.Windows.Forms.Button RentMgrBtn;
+        private System.Windows.Forms.Button CustomMgrBtn;
+        private System.Windows.Forms.Button CatMgrBtn;
+        private System.Windows.Forms.Button EquipMgrBtn;
+        private System.Windows.Forms.Label OperatorText;
+        private System.Windows.Forms.Label OperatorLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button DisplayAll;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
