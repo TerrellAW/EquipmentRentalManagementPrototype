0233b31616d3621cfd672cb6a5c63579186d46ee
Ruili Hu <ruili.hu@edu.sait.ca>
2024-12-12 02:29:16 -0700

    Create DB connection

---
+++ database/DBconnector.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace EquipmentRentalManagementPrototype.database
+{
+    internal class DBconnector
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBconnector()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.ErrorCode)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server. Contact administrator");
+                        break;
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public List<Dictionary<string, object>> ExecuteQuery (string query)
+        {
+            List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+
+            if (OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Dictionary<string, object> row = new Dictionary<string, object>();
+                    for (int i = 0; i < dataReader.FieldCount; i++)
+                    {
+                        row[dataReader.GetName(i)] = dataReader.GetValue(i);
+                    }
+                    result.Add(row);
+                }
+                dataReader.Close();
+                CloseConnection();
+            }
+            return result;
+        }
+
+        public void ExecuteCommand(string query)
+        {
+            if (OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                CloseConnection();
+            }
+        }
+    }
+}
